18ede7716a6fcc9ffbd5fca8aa34cc6c84a7a5a9
amit-techuplabs <155612508+amit-techuplabs@users.noreply.github.com>
2024-12-22 21:07:14 +0530

    refactor: enhance UI layout and add notification count feature

--- renderer.js
+++ renderer.js
@@ -1,10 +1,13 @@
 let intervalId;
 let isRunning = false;
+let notificationCount = 0;
 
 document.getElementById("start-stop-button").addEventListener("click", () => {
   const button = document.getElementById("start-stop-button");
   const messages = document.getElementById("messages").value.split(",");
   const interval = parseInt(document.getElementById("interval").value) * 1000;
+  const notificationCountElement =
+    document.getElementById("notification-count");
 
   if (isRunning) {
     clearInterval(intervalId);
@@ -17,10 +20,15 @@ document.getElementById("start-stop-button").addEventListener("click", () => {
       return;
     }
 
+    notificationCount = 0;
+    notificationCountElement.innerText = `Notifications fired: ${notificationCount}`;
+
     intervalId = setInterval(() => {
       const message =
         messages[Math.floor(Math.random() * messages.length)].trim();
       new Notification("Notification", { body: message });
+      notificationCount++;
+      notificationCountElement.innerText = `Notifications fired: ${notificationCount}`;
     }, interval);
 
     button.innerText = "Stop";
