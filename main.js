d112778a0f80a1b6fff0444f11b24ac0adcfbc68
amit-techuplabs <155612508+amit-techuplabs@users.noreply.github.com>
2024-12-22 20:59:53 +0530

    refactor: update UI elements and styles for AnnoPing app

--- main.js
+++ main.js
@@ -5,13 +5,14 @@ const path = require("node:path");
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 400,
+    height: 400,
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
       nodeIntegration: true,
       contextIsolation: false,
     },
+    autoHideMenuBar: true, // Add this line to hide the menu bar
   });
 
   // and load the index.html of the app.
